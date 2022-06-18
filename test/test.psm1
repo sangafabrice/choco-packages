043307c1155809ae54557d4b9faf1c800c967e6c
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-18 19:45:12 +0200

    fix compability with Powershell v2

--- test/test.psm1
+++ test/test.psm1
@@ -1,16 +1,16 @@
-Class _ {
-    Static [psobject] PushToDist([scriptblock] $ScriptBlock) {
-        Push-Location '~\Desktop\dist'
-        $Result = & $ScriptBlock
-        Pop-Location
-        Return $Result
-    }
+$PushToDist = {
+    Param ([scriptblock] $ScriptBlock = {})
+    Push-Location '~\Desktop\dist'
+    & $ScriptBlock
+    Pop-Location
 }
 
-Filter Get-Package { [_]::PushToDist({ (Get-Item ".\*.nuspec").Name -replace '\.nuspec$' }) }
+Filter Get-Package { & $PushToDist { (Get-Item ".\*.nuspec").Name -replace '\.nuspec$' } }
 
-Filter Test-Install { [_]::PushToDist({ choco install (Get-Package) --source "'.;https://community.chocolatey.org/api/v2/'" --yes --force }) }
+Filter Test-Install { & $PushToDist { choco install (Get-Package) --source "'.;https://community.chocolatey.org/api/v2/'" --yes --force } }
 
 Filter Test-Upgrade { choco upgrade (Get-Package) --yes --force }
 
 Filter Test-Uninstall { choco uninstall (Get-Package) --yes --force }
+
+Export-ModuleMember -Function '*'
