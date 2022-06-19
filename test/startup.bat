cc2edcf6b94a8be4d4c310465c12f45d95cdf308
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-19 22:49:34 +0200

    add version attribute to startup script

--- test/startup.bat
+++ test/startup.bat
@@ -1,9 +1,13 @@
+@Echo OFF
 PushD "%USERPROFILE%\Desktop"
+SetLocal
 Powershell -Command "& { Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force }"
 Powershell -Command "& { [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) }"
 :Loop
 Rem
 If Not EXIST %ProgramData%\Chocolatey GoTo Loop
 Set Path=%ProgramData%\Chocolatey;%Path%
-Start /Max Powershell -NoExit -Command "Import-Module .\test -Prefix 'Choco'"
+If /I '%USERNAME%' NEQ 'WDAGUtilityAccount' Set VersionAttribute=-Version 2.0
+Start /Max Powershell %VersionAttribute% -NoExit -Command "Import-Module .\test -Prefix 'Choco'"
+EndLocal
 PopD
