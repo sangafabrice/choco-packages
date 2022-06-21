536f943da938cdd1ebe0eb737743c38204e1d0aa
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-21 13:55:15 +0200

    add verbose argument to tests

--- test/test.psm1
+++ test/test.psm1
@@ -7,9 +7,9 @@ $PushToDist = {
 
 Filter Get-Package { & $PushToDist { (Get-Item ".\*.nuspec").Name -replace '\.nuspec$' } }
 
-Filter Test-Install { & $PushToDist { choco install (Get-Package) --source "'.;https://community.chocolatey.org/api/v2/'" --yes --force } }
+Filter Test-Install { & $PushToDist { choco install (Get-Package) --source "'.;https://community.chocolatey.org/api/v2/'" -dv --yes --force } }
 
-Filter Test-Upgrade { choco upgrade (Get-Package) --yes --force }
+Filter Test-Upgrade { choco upgrade (Get-Package) --source "'.;https://community.chocolatey.org/api/v2/'" --yes --force }
 
 Filter Test-Uninstall { choco uninstall (Get-Package) --yes --force }
 
